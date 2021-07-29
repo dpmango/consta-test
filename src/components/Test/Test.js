02b847468a128e911ee414d278ad7021fe28ff8c
Sergey Khmelevskoy <xs290@me.com>
2021-07-29 18:53:09 +0300

    cra template

---
+++ src/components/Test/Test.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Button } from '@consta/uikit/Button';
+
+import './App.scss';
+
+const Test = () => {
+  return (
+    <div className="container">
+      <Button label="Кнопка" />
+    </div>
+  );
+}
+
+export default Test
--- src/index.js
+++ src/index.js
@@ -1,17 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Theme, presetGpnDefault } from '@consta/uikit/Theme';
+
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import TestTask from './components/Test';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Theme preset={presetGpnDefault}>
+      <TestTask />
+    </Theme>
   </React.StrictMode>,
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
